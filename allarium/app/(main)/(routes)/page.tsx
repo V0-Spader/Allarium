9419a6bda3b35e6ae7412fd471bb9bf6cc7c85f6
Vo Spader <Travisschaak@Gmail.com>
2024-04-06 15:33:14 -0400

    setting up auth

--- allarium/app/(main)/(routes)/page.tsx
+++ allarium/app/(main)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
   return (
-    <div>
-      This is a protected route.
+    <div className="h-screen">
+      <UserButton 
+        afterSignOutUrl="/"
+      />
     </div>
   )
 }
