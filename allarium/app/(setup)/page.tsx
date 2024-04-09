c4baa7a8661202f8d9a3aa12e8d2bfb5589e5d75
Vo Spader <Travisschaak@Gmail.com>
2024-04-08 22:16:03 -0400

    swtuched to supabase db w/ postgresql and finished setup

---
+++ allarium/app/(setup)/page.tsx
@@ -0,0 +1,25 @@
+import { innitialProfile } from "@/lib/initial-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const  SetupPage = async () => {
+    const profile = await innitialProfile();
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) {
+        return redirect(`/servers/${server.id}`);
+    }
+
+    return <div>Create a Server</div>;
+}
+ 
+export default SetupPage;
